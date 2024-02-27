6645316c01ec609823ae8719c5f51a89dbc4d3a5
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-02-26 22:24:11 -0300

    Classify and predict labels

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn import svm
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 st = PorterStemmer()
+MINIMUM_RATINGS = 30
+
+GOOD_RATING = 4.5
+MEDIUM_RATING = 4
 
 # stemming and stop words removal
 def simplify_text(text):
@@ -24,38 +29,67 @@ def simplify_text(text):
     simple = [st.stem(x) for x in simple]
     return simple
 
+def discretize_rating(rating):
+    if rating >= GOOD_RATING:
+        return 1
+    # elif rating >= MEDIUM_RATING:
+    #     return 1
+    else: return 0
+    
+def linear_regression(X_train, y_train, X_test, y_test):
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    
+    errors = []
+    for i, y in enumerate(y_test):
+        error = abs(y - y_pred[i])
+        errors.append(error)
+        
+    print(errors)
+    sum = 0
+    for e in errors:
+        sum += e
+    print(sum)
+
+def classifier(X_train, y_train, X_test, y_test):
+    clf = svm.SVC(kernel='linear')
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    cm = confusion_matrix(y_test, y_pred, labels=clf.classes_)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf.classes_)
+    disp.plot()
+    plt.show()
+
 def main():
     nltk.download()
     df = pd.read_csv("./games.csv")
-    df['description_wcount'] = df.description.apply(lambda d : len(re.findall(r'\w+', d)))
-    df['description_ccount'] = df.description.apply(len)
-    # df['description_simple'] = df.description.apply(simplify_text)
+    df = df[df.userRatingCount > MINIMUM_RATINGS]
+    df['descriptionWCount'] = df.description.apply(lambda d : len(re.findall(r'\w+', d)))
+    df['descriptionCCount'] = df.description.apply(len)
+    df['goodRating'] = df.averageUserRating.apply(discretize_rating)
+    # df['descriptionSimple'] = df.description.apply(simplify_text)
     
     vectorizer = CountVectorizer(analyzer=simplify_text).fit(df.description)
     vector = vectorizer.transform(df.description)
     vectorDF = pd.DataFrame(vector.todense(), columns=vectorizer.get_feature_names_out())
-    vectorDF = pd.concat([df.averageUserRating, vectorDF], axis=1)
+    vectorDF = pd.concat([df.goodRating, vectorDF], axis=1)
     print("Shape of vectorized DF:", vectorDF.shape)
     print(vectorDF.head(10))
     
     vectorizerTF = TfidfTransformer().fit(vector)
     vectorTF = vectorizerTF.transform(vector)
     vectorTFDF = pd.DataFrame(vectorTF.todense(), columns=vectorizer.get_feature_names_out())
-    vectorTFDF = pd.concat([df.averageUserRating, vectorTFDF], axis=1)
+    vectorTFDF = pd.concat([df.goodRating, vectorTFDF], axis=1)
     print("Shape of vector TFDF:", vectorTFDF.shape)
     print(vectorTFDF.head(10))
     
-    X = vectorTFDF.drop('averageUserRating', axis=1)
-    y = vectorTFDF.averageUserRating
+    X = vectorTFDF.drop('goodRating', axis=1)
+    y = vectorTFDF.goodRating
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
     
-    cm = confusion_matrix(y_test, y_pred)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot()
-    plt.show()
+    classifier(X_train, X_test, y_train, y_test)
     
 if __name__ == "__main__":
     main()
